dcf777e36a0e9a025f09e28821ef0dad2d989175
Galen Flanagan <flanagg21@vt.edu>
2024-04-30 14:47:04 +0000

    I don't know

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -16,7 +16,6 @@ class IsingHamiltonian:
         """
         self.J = J
         self.bs = BitString(len(mus))
-        self.mu = 1
         if isinstance(mus, list):
             self.bs.set_config(mus)
         else:    
